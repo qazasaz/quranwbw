d9fb6ef514702ca6de8491db90875a2e6d7a2ea5
marwanansari <it.is.marwan@gmail.com>
2019-07-16 15:05:11 +0400

    Updated jQuery and Bootstrap to the newest versions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v1.80';
-const RUNTIME = 'runtime-v1.80';
+const PRECACHE = 'precache-v1.85';
+const RUNTIME = 'runtime-v1.85';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [

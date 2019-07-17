7fa8e762249729eceefa4e5977cbdc858194e482
marwanansari <it.is.marwan@gmail.com>
2019-07-18 00:34:17 +0400

    Minor homepage design changes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v1.85';
-const RUNTIME = 'runtime-v1.85';
+const PRECACHE = 'precache-v1.92';
+const RUNTIME = 'runtime-v1.92';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
